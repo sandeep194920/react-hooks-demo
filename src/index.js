1b37b408b780499a3b6f57ee20117344982194fd
sandeep194920 <sandeepamaranath@gmail.com>
2020-04-24 18:57:58 -0400

    delete message added

--- src/index.js
+++ src/index.js
@@ -7,6 +7,20 @@ import AuthContextProvider from "./components/context/auth-context";
 
 ReactDOM.render(
   <AuthContextProvider>
+    <p
+      style={{
+        textAlign: "center",
+        color: "#fff",
+        border: "1px solid #ff2058",
+        background: "#ff2058",
+        marginLeft: "35%",
+        display: "inline-block",
+        padding: "2%",
+        borderRadius: "0%",
+      }}
+    >
+      You can delete an item by clicking on it!
+    </p>
     <App />
   </AuthContextProvider>,
   document.getElementById("root")
